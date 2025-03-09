569f18036c82947fb198271bc8fcb558d1792a1f
31628 <vic_chang@everrich.com.tw>
2025-03-09 22:08:44 +0800

    新增時段

--- mybackend/myapp/admin.py
+++ mybackend/myapp/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from .models import Animal, Hall
 
-admin.site.register(Animal)
-admin.site.register(Hall)
+@admin.register(Animal)
+class AnimalAdmin(admin.ModelAdmin):
+    # 列表頁面只顯示「名稱」、「時段」、「物種」、「年齡」與「館別」
+    list_display = ('name', 'time_slot', 'species', 'age', 'hall')
+    search_fields = ('name', 'species', 'time_slot')
+    # 編輯表單中僅包含這些欄位，不包含體重
+    fields = ('name', 'time_slot', 'age', 'species', 'hall', 'photo')
+
+@admin.register(Hall)
+class HallAdmin(admin.ModelAdmin):
+    list_display = ('name',)
--- mybackend/myapp/models.py
+++ mybackend/myapp/models.py
@@ -10,9 +10,14 @@ class Animal(models.Model):
     name = models.CharField("動物名稱", max_length=100)
     age = models.IntegerField("年齡")
     species = models.CharField("物種", max_length=100)
-    weight = models.FloatField("體重")
     hall = models.ForeignKey(Hall, verbose_name="館別", on_delete=models.SET_NULL, null=True, blank=True)
     photo = models.ImageField("照片", upload_to='animal_photos/', blank=True, null=True)
+    time_slot = models.CharField(
+        "時段",
+        max_length=200,
+        blank=True,
+        help_text="請輸入時段，每個時段用 '.' 隔開，例如：12.13.14.15.16.17.18.19.20"
+    )
 
     def __str__(self):
         return self.name
