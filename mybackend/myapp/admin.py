3b55513740a9d5b6d58802833756667c92c44ca1
31628 <vic_chang@everrich.com.tw>
2025-03-10 02:58:02 +0800

    表格/後台欄位/合併身材/完美版本

--- mybackend/myapp/admin.py
+++ mybackend/myapp/admin.py
@@ -3,11 +3,9 @@ from .models import Animal, Hall
 
 @admin.register(Animal)
 class AnimalAdmin(admin.ModelAdmin):
-    # 更新為新的欄位名稱（size, fee）
-    list_display = ('name', 'time_slot', 'size', 'fee', 'hall')
-    search_fields = ('name', 'size', 'time_slot')
-    # 修改 fields，確保不包含已刪除的欄位
-    fields = ('name', 'time_slot', 'fee', 'size', 'hall', 'photo')
+    list_display = ('name', 'time_slot', 'size_display', 'fee', 'hall')
+    search_fields = ('name', 'height', 'weight', 'cup_size', 'time_slot')
+    fields = ('name', 'height', 'weight', 'cup_size', 'fee', 'time_slot', 'hall', 'photo')
 
 @admin.register(Hall)
 class HallAdmin(admin.ModelAdmin):
--- mybackend/myapp/models.py
+++ mybackend/myapp/models.py
@@ -8,16 +8,23 @@ class Hall(models.Model):
 
 class Animal(models.Model):
     name = models.CharField("動物名稱", max_length=100)
-    fee = models.IntegerField("台費", default=0)  # 原本是 `age`
-    size = models.CharField("身材", max_length=100, default="")  # 原本是 `species`
-    hall = models.ForeignKey(Hall, verbose_name="館別", on_delete=models.SET_NULL, null=True, blank=True)
-    photo = models.ImageField("照片", upload_to='animal_photos/', blank=True, null=True)
+    height = models.IntegerField("身高", blank=True, null=True)  # 身高
+    weight = models.IntegerField("體重", blank=True, null=True)  # 體重
+    cup_size = models.CharField("罩杯", max_length=5, blank=True, null=True)  # 罩杯
+    fee = models.IntegerField("台費", blank=True, null=True)  # 台費
     time_slot = models.CharField(
         "時段",
         max_length=200,
         blank=True,
         help_text="請輸入時段，每個時段用 '.' 隔開，例如：12.13.14.15.16.17.18.19.20"
     )
+    hall = models.ForeignKey(Hall, verbose_name="館別", on_delete=models.SET_NULL, null=True, blank=True)
+    photo = models.ImageField("照片", upload_to='animal_photos/', blank=True, null=True)
 
     def __str__(self):
         return self.name
+
+    @property
+    def size_display(self):
+        """組合身高.體重.罩杯"""
+        return f"{self.height}.{self.weight}.{self.cup_size}" if self.height and self.weight and self.cup_size else ""
