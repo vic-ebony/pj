5165feba9c3c9ade9212c352e3acf24454b3717f
vic-ebony <holyshake33@gmail.com>
2025-03-15 15:11:56 +0000

    更新 Django 設定與靜態檔案設定

--- mybackend/mybackend/settings.py
+++ mybackend/mybackend/settings.py
@@ -131,8 +131,6 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-# 確保不使用 collectstatic
-STATIC_ROOT = None
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
